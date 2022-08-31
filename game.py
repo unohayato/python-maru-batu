d65595e96784b5c44c91fecce539114c88829c45
unohayato <mamidori1p1p@gmail.com>
2022-08-31 19:40:30 +0900

    our_select

--- game.py
+++ game.py
@@ -1,36 +1,85 @@
 
 
-
-
-
 field = [
-  ["0", "1","2",],
-  ["3", "4","5",],
-  ["6", "7","8",],
+  ["0", "1", "2"],
+  ["3", "4", "5"],
+  ["6", "7", "8"]
   ]
-for i in field:
-  print(i)
+
+#フィールド描画
+def draw_field(field):
+  print("---------------------")
+  for i in field:
+    print(i)
+
+#適切に選択して描画
+def our_select(field):
+
+  while True:
+    s = int(input("マスの番号を選択してください。"))
+    
+    if s == 0 or s == 1 or s == 2:
+      if field[0][s] == "●" or field[0][s] == "✖️":
+        print('空いている適切なマスを選択してください。')
+        continue
+      else:
+        field[0][s] = "●"
+        break
+      
+    elif s == 3 or s == 4 or s == 5:
+      if field[1][s-3] == "●" or field[1][s-3] == "✖️":
+        print('空いている適切なマスを選択してください。')
+        continue
+      else:
+        field[1][s-3] = "●"
+        break
+      
+    elif s == 6 or s == 7 or s == 8:
+      if field[2][s-6] == "●" or field[2][s-6] == "✖️":
+        print('空いている適切なマスを選択してください。')
+        continue
+      else:
+        field[2][s-6] = "●"
+        break
+    else:
+      print('空いている適切なマスを選択してください。')
+      continue
+    
+    
+  return field
+        
+      
+
+  
+        
+  
+  
 
 
 #縦、横、斜めのどれかが揃っていたらTrueそれ以外False
 def judge(field):
-  ret = False
+  
   for x in field: #縦
     if x[0] == x[1] == x[2]:
-      ret = True
+      return True
       
   for y in range(3):#横
     if field[y][0] == field[y][1] == field[y][2]:
-      ret = True
+      return True
       
   if field[0][0] == field[1][1] == field[2][2]:#右下斜線
-    ret = True
+    return True
   
   if field[0][2] == field[1][1] == field[2][0]:#右上斜線
-    ret = True
+    return True
   
-  return ret
 
-field[0] = ["●", "●", "✖️"]
 
-print(judge(field))
+while True:
+  draw_field(field)
+  
+  our_select(field)
+  
+  draw_field(field)
+  
+  
